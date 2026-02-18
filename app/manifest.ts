0dd2a820e132759eb0856e183f52556a065b8a32
Chris YESSO <yessochrisa@gmail.com>
2026-02-18 14:43:12 +0000

    feat(main): changed favicon and pwa update

--- app/manifest.ts
+++ app/manifest.ts
@@ -17,7 +17,12 @@ export default function manifest(): MetadataRoute.Manifest {
       { src: "/icons/144.png", sizes: "144x144", type: "image/png" },
       { src: "/icons/152.png", sizes: "152x152", type: "image/png" },
       { src: "/icons/192.png", sizes: "192x192", type: "image/png" },
-      { src: "/icons/512.png", sizes: "512x512", type: "image/png", purpose: "any maskable" },
+      {
+        src: "/icons/512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
     ],
   };
 }
